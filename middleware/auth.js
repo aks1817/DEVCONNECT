24c5e2e0b6a7d6940d3bf8efee18760429752061
Anurag Shukla <anuragshukla@Anurags-MacBook-Air.local>
2023-01-07 12:17:14 +0530

    Done with user Api

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt=require('jsonwebtoken');
+const config=require('config');
+const jwtSecret=config.get('jwtSecret');
+
+const auth=(req,res,next)=>{
+    //Get token from header
+    const token =req.header('x-auth-token');
+
+    //Check if not token
+    if(!token){
+        return res.status(401).json({msg:'No token, authorization denied'});
+    }
+
+    //Verify token
+
+    try {
+        const decode=jwt.verify(token,jwtSecret);
+
+        req.user=decode.user;
+        next();
+    } catch (err) {
+        res.status(401).json({msg:"Token is not valid"});
+    }
+}
+
+
+module.exports=auth;
