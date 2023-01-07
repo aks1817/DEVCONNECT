74ce5149ab29a1c1194f3ce359d65db2af7aec0e
Anurag Shukla <anuragshukla@Anurags-MacBook-Air.local>
2023-01-07 17:15:22 +0530

    Create and update profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,106 @@
-const  express=require('express');
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+const { check, validationResult } = require("express-validator");
+// @route GET api/profile/me
+// @desc  Get current users profile
+// @access Private
 
-// @route GET api/profile
-// @desc Test route
+router.get("/me", auth, async (req, res) => {
+  try {
+    /* First we find Profile on the basis of userID and the since the user atribute of ProfileSchema is refrenced to UserSchema so we populated data say name and avatar to profile variable
+     */
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route POST api/profile
+// @desc  Create or update user profile
 // @access Public
 
-router.get('/',(req,res)=>res.send('Profile route'));
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "Status is required").not().isEmpty(),
+      check("skills", "Skills is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // destructure the request
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body;
 
-module.exports=router;
+    //Build profile object
+
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      // If already exists then update
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true } // You should set new option to true to return the document after update was applied
+        );
+        return res.json(profileFields);
+      }
+      //Else create a new profile
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,67 +1,77 @@
-const express=require('express');
-const router=express.Router();
-const gravatar=require('gravatar');
-const bcrypt=require('bcryptjs');
-const User=require('../../models/User');
-const jwt=require('jsonwebtoken');
-const config=require('config');
-const jwtSecret=config.get('jwtSecret');
-const {check,validationResult}=require('express-validator');
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const jwtSecret = config.get("jwtSecret");
+const { check, validationResult } = require("express-validator");
 // @route POST api/users
 // @desc  Register user
 // @access Public
 
-router.post('/',[check('name','Name is required').not().isEmpty(),
-check('email','Please include a valid email').isEmail(),
-check('password','Please enter a password with 6 or  more characters').isLength({min:6})],async (req,res)=>{
-    const errors=validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or  more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const {name,email,password}=req.body;   // destructuring req body
+    const { name, email, password } = req.body; // destructuring req body
     try {
-        //1) See if user exists
-        let user = await User.findOne({email:email});  // this function returns promise so we added async before (req,res) and in find one we write the paramter on which we want our search
-        if(user){   // If user already exists
-            res.status(400).json({errors:[{msg:'User already exists'}]});
-        }
-        //2) Get user gravatar
-        //If user doesn't exists grab its gravatar
-        const avatar=gravatar.url(email,{
-            s:'200', //size
-            r: 'pg',
-            d: 'mm'  // default image
-        })
-        
-        user=new User({   // creating user object if doesn't exists
-            name,
-            email,
-            avatar,
-            password
-        });
+      //1) See if user exists
+      let user = await User.findOne({ email: email }); // this function returns promise so we added async before (req,res) and in find one we write the paramter on which we want our search
+      if (user) {
+        // If user already exists
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+      //2) Get user gravatar
+      //If user doesn't exists grab its gravatar
+      const avatar = gravatar.url(email, {
+        s: "200", //size
+        r: "pg",
+        d: "mm", // default image
+      });
 
-        //3) Encrypt password and saving to MongoDB
-        const salt=await bcrypt.genSalt(10);
-        user.password=await bcrypt.hash(password,salt);
-        await user.save();  // saving data to MongoDB
+      user = new User({
+        // creating user object if doesn't exists
+        name,
+        email,
+        avatar,
+        password,
+      });
 
+      //3) Encrypt password and saving to MongoDB
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save(); // saving data to MongoDB
 
-        //4) Return jsonwebtoken
-        const payload={
-            user:{
-                id:user.id
-            }
-        }
+      //4) Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-        jwt.sign(payload,jwtSecret,{expiresIn:36000},(err,token)=>{
-            if(err) throw err;
-            res.json({token});
-        });
+      jwt.sign(payload, jwtSecret, { expiresIn: 36000 }, (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      });
     } catch (err) {
-        console.log(err.message);
-        res.send(500).send('Server error');
+      console.log(err.message);
+      res.send(500).send("Server error");
     }
-});
+  }
+);
 
-module.exports=router;
+module.exports = router;
