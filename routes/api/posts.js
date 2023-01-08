c02470a91fa461673d5b9b4bb99a4a1457a288e1
Anurag Shukla <anuragshukla@Anurags-MacBook-Air.local>
2023-01-08 13:33:10 +0530

    Add and Delete comment on post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -145,4 +145,73 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// @route PUT api/posts/comment/:id
+// @desc  Comment on a post
+// @access Private
+
+router.put(
+  "/comment/:id",
+  [auth, check("text", "Text is required").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      const post = await Post.findById(req.params.id);
+      const user = await User.findOne({ _id: req.user.id }).select("-password");
+      if (!post) {
+        return res.status(400).json({ msg: "Post doesn't exists" });
+      }
+      const newComment = {
+        user: req.user.id,
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+      };
+      post.comments.unshift(newComment);
+      post.save();
+      res.json(post);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc  Delete a comment on post
+// @access Private
+
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Pull out comment
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    //Check comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exists" });
+    }
+    //Check user deletes its own comment
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not Authorized" });
+    }
+
+    const removeIndex = post.comments
+      .map((item) => item.id)
+      .indexOf(req.params.comment_id);
+
+    post.comments.splice(removeIndex, 1);
+    post.save();
+    res.json(post);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
