a23391fccbd0f318d912db074e02322642464d1d
Anurag Shukla <anuragshukla@Anurags-MacBook-Air.local>
2023-01-15 15:53:52 +0530

    Get loggedIn user profile data in dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
-
-const Dashboard = () => {
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
+const Dashboard = ({ profile, getCurrentProfile, auth }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+  console.log(profile, auth);
   return <div className="container">Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
-
-export default Dashboard;
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  profile: state.profile.profile,
+  auth: state.auth.user,
+});
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
